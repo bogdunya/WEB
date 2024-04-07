af3b07997dba89ca5751f2382ce064c6f16c6d95
Shvidky Bogdan <bogdunya@mail.ru>
2024-04-07 04:29:51 +0300

    main готов

--- js/man.js
+++ js/man.js
@@ -2,3 +2,20 @@ function changeImage(imageUrl) {
     document.getElementById('mainImage').src = imageUrl;
   }
   
+
+  var collapseList = document.querySelectorAll('.collapse');
+  var collapseTriggers = document.querySelectorAll('[data-bs-toggle="collapse"]');
+
+  collapseTriggers.forEach(function (triggerEl) {
+    triggerEl.addEventListener('click', function () {
+      var target = document.querySelector(triggerEl.getAttribute('data-bs-target'));
+
+      collapseList.forEach(function (collapseEl) {
+        if (collapseEl !== target) {
+          var collapse = bootstrap.Collapse.getInstance(collapseEl);
+          if (collapse && collapse._isTransitioning) return;
+          collapse.hide();
+        }
+      });
+    });
+  });
